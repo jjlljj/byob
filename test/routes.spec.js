7c10f4c207f0e8a2203920b2f55d4397804cf504
James Logue <logueja@gmail.com>
2018-03-26 14:06:29 -0600

    Add passing test mock

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,12 +4,15 @@ const should = chai.should()
 const chaiHttp = require('chai-http')
 const server = require('../server')
 
-chai.use(chaiHttps)
+chai.use(chaiHttp)
 
 const environment = process.env.NODE_ENV || 'test'
 const configuration = require('../knexfile')[environment]
 const db = require('knex')(configuration)
 
 describe('Client Routes', () => {
+  it('should be a passing test suite', () => {
+    (true).should.equal(true)
+  })
 
-}
+})
