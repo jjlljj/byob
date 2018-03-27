f9d464503ae2846ec36c9e6736cf1a7a18598bd8
Jeff Goodall <goodallhj@gmail.com>
2018-03-27 13:05:44 -0600

    adding data folder with resources

--- db/migrations/20180326134753_initial.js
+++ db/migrations/20180326134753_initial.js
@@ -1,8 +1,30 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('groups', function(table) {
+      table.increments('id').primary();
+      table.string('group');
+      table.string('gender');
+      table.string('age');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('years', function(table) {
+      table.increments('id').primary();
+      table.string('unemployment_score');
+      table.string('year');
+      table.integer('group_id').unsigned();
+      table.foreign('group_id').references('groups.id');
+
+      table.timestamps(true, true);
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('years'),
+    knex.schema.dropTable('groups')
+  ]);
 };
