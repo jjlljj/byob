cabd6900080fa6095e5f5545387c0ace33a4842f
Jeff Goodall <goodallhj@gmail.com>
2018-03-29 12:52:41 -0600

    appending token and sucessful fetch of token

---
+++ public/scripts/script.js
@@ -0,0 +1,29 @@
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', event => {
+  event.preventDefault();
+  const app_name = document.querySelector('#app').value;
+  const email = document.querySelector('#email').value;
+  
+  fetch('/authorize', {
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({app_name, email}),
+    method: 'POST'
+  })
+    .then(response => response.json())
+    .then(json => {
+      append(json);
+    });
+});
+
+const append = (token) => {
+  const container = document.querySelector('#token');
+  const parent = document.createElement("div");
+  parent.setAttribute('class', 'card');
+  parent.innerHTML= `
+  <h2>Token: ${token.token}</h2>
+  `
+  container.append(parent);
+};
